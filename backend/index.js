c0399971cb960d319b7af2417b1f7f0c69245d98
ronakpanwar <ronakpanwar18@gmail.com>
2024-11-12 19:06:07 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: "http://localhost:3000",
+    origin: `${process.env.FRONTEND_URL}`,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 };
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin:`${process.env.FRONTEND_URL}`,
         methods: ["GET", "POST"],
         credentials: true
     }
